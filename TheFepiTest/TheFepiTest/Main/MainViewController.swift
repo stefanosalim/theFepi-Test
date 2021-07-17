9f5ec2de647809c825c4096ed36e988169dd1244
stefano.salim <stefano.salim@traveloka.com>
2021-07-17 16:25:25 +0700

    set basic setting for TheFepi-test

---
+++ TheFepiTest/TheFepiTest/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  TheFepiTest
+//
+//  Created by stefano.salim on 17/07/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
