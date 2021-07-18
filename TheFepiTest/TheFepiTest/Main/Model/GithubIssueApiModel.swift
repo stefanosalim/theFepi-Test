5eea0cda2bc99fb737a09ef7d329b1165bd0b5d0
stefano.salim <stefano.salim@traveloka.com>
2021-07-19 06:18:26 +0700

    add API Model

--- TheFepiTest/TheFepiTest/Main/Model/GithubIssueApiModel.swift
+++ TheFepiTest/TheFepiTest/Main/Model/GithubIssueApiModel.swift
@@ -13,3 +13,25 @@ struct GithubIssueSpec {
     var page: Int
     var perPage: Int
 }
+
+struct GithubIsseResponse: Decodable {
+    var items: [GithubIssueItem]
+}
+
+struct GithubIssueItem: Decodable {
+    
+    var title: String
+    var updatedAt: String
+    var state: String
+    
+    enum DefaultIssueKeys: String, CodingKey {
+        case title, state, updatedAt = "updated_at"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let defaultValues = try decoder.container(keyedBy: DefaultIssueKeys.self)
+        title = try defaultValues.decode(String.self, forKey: .title)
+        state = try defaultValues.decode(String.self, forKey: .state)
+        updatedAt = try defaultValues.decode(String.self, forKey: .updatedAt)
+    }
+}
--- TheFepiTest/TheFepiTest/Main/Model/GithubRepoApiModel.swift
+++ TheFepiTest/TheFepiTest/Main/Model/GithubRepoApiModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+struct GithubRepoSpec {
+    
+    var query: String
+    var page: Int
+    var perPage: Int
+}
+
+struct GithubRepoResponse: Decodable {
+    var items: [GithubRepoItem]
+}
+
+struct GithubRepoItem: Decodable {
+    
+    var name: String
+    var createdAt: String
+    var watchers: Int
+    var forks: Int
+    var stars: Int
+    
+    enum DefaultIssueKeys: String, CodingKey {
+        case name, watchers, forks, createdAt = "created_at", stars = "stargazers_count"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let defaultValues = try decoder.container(keyedBy: DefaultIssueKeys.self)
+        name = try defaultValues.decode(String.self, forKey: .name)
+        createdAt = try defaultValues.decode(String.self, forKey: .createdAt)
+        stars = try defaultValues.decode(Int.self, forKey: .stars)
+        watchers = try defaultValues.decode(Int.self, forKey: .watchers)
+        forks = try defaultValues.decode(Int.self, forKey: .forks)
+    }
+}
